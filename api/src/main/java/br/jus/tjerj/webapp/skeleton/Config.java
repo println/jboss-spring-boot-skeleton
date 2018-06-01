41c12d77ac9a9d26033cbc1cda503de31f179e2e
println <live.proto@hotmail.com>
2018-05-31 23:02:39 -0300

    Inclusao de bean condicional por conta de existencia do jndi

--- api/src/main/java/br/jus/tjerj/webapp/skeleton/Config.java
+++ api/src/main/java/br/jus/tjerj/webapp/skeleton/Config.java
@@ -2,13 +2,16 @@ package br.jus.tjerj.webapp.skeleton;
 
 import javax.sql.DataSource;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnJndi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 
 @Configuration
 public class Config {
-	//@Bean
+	
+	@Bean
+	@ConditionalOnJndi
 	public DataSource dataSource() 
 	{
 	  JndiDataSourceLookup dataSourceLookup = new JndiDataSourceLookup();
--- api/src/main/java/br/jus/tjerj/webapp/skeleton/student/StudentNotFoundException.java
+++ api/src/main/java/br/jus/tjerj/webapp/skeleton/student/StudentNotFoundException.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class StudentNotFoundException extends RuntimeException {
 
+	private static final long serialVersionUID = -2761084141068051723L;
+
 	public StudentNotFoundException(String exception) {
 		super(exception);
 	}
