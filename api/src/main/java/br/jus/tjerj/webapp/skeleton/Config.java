d778622039d76c461db7e50f922f9fc027f278b5
println <live.proto@hotmail.com>
2018-05-31 22:21:22 -0300

    Incluindo integracao com jboss system-property e datasource

---
+++ api/src/main/java/br/jus/tjerj/webapp/skeleton/Config.java
@@ -0,0 +1,19 @@
+package br.jus.tjerj.webapp.skeleton;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
+
+@Configuration
+public class Config {
+	//@Bean
+	public DataSource dataSource() 
+	{
+	  JndiDataSourceLookup dataSourceLookup = new JndiDataSourceLookup();
+	  DataSource dataSource = dataSourceLookup.getDataSource("java:jboss/datasources/ExampleDS");
+	  System.out.println("SPRING-BOOT: OBTENDO DATASOURCE DO JBOSS");
+	  return dataSource;
+	}
+}
---
+++ api/src/main/java/br/jus/tjerj/webapp/skeleton/environment/EnvironmentResource.java
@@ -0,0 +1,36 @@
+package br.jus.tjerj.webapp.skeleton.environment;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+public class EnvironmentResource {
+
+	@Value("${environment}")
+	private String environment;
+
+	@GetMapping("/environment")
+	public String retrieveEnvironment() {
+		return environment;
+	}
+}
